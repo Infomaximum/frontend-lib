70d4dc57b9271b4cfb81019ce4ebee6e5d678b21
Alexandr Petyaykin <saneksa2@gmail.com>
2024-03-22 10:34:09 +0300

    feat: добавлены модули svg

--- libs/global-types/src/index.ts
+++ libs/global-types/src/index.ts
@@ -1,16 +1,25 @@
-declare global {
-  interface Infomaximum {
-    system: {
-      /** Префикс пути для загрузки ресурсов */
-      apiPrefix: string;
-      /** Префикс для разделения пути для загрузки данных */
-      basePrefix: string;
-    };
-  }
-
-  interface Window {
-    im: Infomaximum;
-  }
+interface Infomaximum {
+  system: {
+    /** Префикс пути для загрузки ресурсов */
+    apiPrefix: string;
+    /** Префикс для разделения пути для загрузки данных */
+    basePrefix: string;
+  };
 }
 
-export {};
+interface Window {
+  im: Infomaximum;
+}
+
+/** Импортировать svg в base64 */
+declare module "*.svg?url" {
+  const srcSvgUrl: string;
+  export default srcSvgUrl;
+}
+
+/** Импортировать svg как компонент реакта */
+declare module "*.svg" {
+  const srcSvg: React.FC<React.SVGAttributes<SVGElement>>;
+
+  export default srcSvg;
+}
